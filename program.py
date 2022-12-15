802c3edc2b51432c551cf2018ac4e250c905b5ae
DaniilBan <nbe2016@yandex.ru>
2022-12-15 17:29:52 +0300

    circle :)

--- program.py
+++ program.py
@@ -1,42 +1,26 @@
-import sys
-
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
-
-
-class Example(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setGeometry(300, 300, 200, 200)
-        self.setWindowTitle('Рисование')
-        self.btn = QPushButton('Рисовать', self)
-        self.btn.move(70, 150)
-        self.do_paint = False
-        self.btn.clicked.connect(self.paint)
-
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
-
-    def paint(self):
-        self.do_paint = True
-        self.repaint()
-
-    def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 0, 0))
-        qp.drawRect(30, 30, 120, 30)
-        qp.setBrush(QColor(0, 255, 0))
-        qp.drawRect(30, 60, 120, 30)
-
+import pygame
+from random import randint
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    sys.exit(app.exec())
+    pygame.init()
+    pygame.display.set_caption('Движущийся круг 2')
+    size = width, height = 800, 800
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    w = False
+    x_pos = 0
+    clock = pygame.time.Clock()
+    screen.fill((0, 0, 255))
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                m = [randint(100, 400) for i in range(2)]
+                y = randint(10, 400)
+                pygame.draw.circle(screen, 'yellow', m, y, 4)
+
+        pygame.display.flip()
+
+    pygame.quit()
